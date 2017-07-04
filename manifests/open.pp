b39214a5c20d3607d7a69d20a0c56428faf84bd7
Chris Churas <churas@gmail.com>
2017-07-03 17:15:59 -0700

    removed lockfile and screen

--- manifests/open.pp
+++ manifests/open.pp
@@ -8,10 +8,10 @@ class nbcrtrain::open
 
   Package { ensure => 'installed' }
   $python_deps    = [ 'python2-pip', 'python-psutil']
-  $other_packages = [ 'libXft', 'xorg-x11-xauth', 'screen', 'bzip2', 'which', 'rsync', 'gcc', 'gcc-c++', 'python-devel', 'cmake', 'git', 'unzip', 'tree', 'wget','tcsh','xauth', 'qt' ,'qt-devel', 'opencv', 'opencv-python','numpy','numpy-f2py','scipy' ]
+  $other_packages = [ 'libXft', 'xorg-x11-xauth', 'bzip2', 'which', 'rsync', 'gcc', 'gcc-c++', 'python-devel', 'cmake', 'git', 'unzip', 'tree', 'wget','tcsh','xauth', 'qt' ,'qt-devel', 'opencv', 'opencv-python','numpy','numpy-f2py','scipy' ]
   $mesa_packages  = [ 'mesa-libGL-devel','mesa-libEGL-devel','mesa-libGLES-devel' ]
   $fonts_packages = [ 'xorg-x11-fonts-100dpi','xorg-x11-fonts-75dpi','xorg-x11-fonts-ISO8859-1-100dpi','xorg-x11-fonts-ISO8859-1-75dpi','xorg-x11-fonts-ISO8859-14-100dpi','xorg-x11-fonts-ISO8859-14-75dpi','xorg-x11-fonts-ISO8859-15-100dpi','xorg-x11-fonts-ISO8859-15-75dpi','xorg-x11-fonts-ISO8859-2-100dpi','xorg-x11-fonts-ISO8859-2-75dpi','xorg-x11-fonts-ISO8859-9-100dpi','xorg-x11-fonts-ISO8859-9-75dpi','xorg-x11-fonts-Type1','xorg-x11-fonts-cyrillic','xorg-x11-fonts-ethiopic','xorg-x11-fonts-misc']
-  $pip_packages   = [ 'argparse','psutil','lockfile','chmutil', 'scikit-learn' ]
+  $pip_packages   = [ 'argparse','psutil','chmutil', 'scikit-learn' ]
   package { $python_deps: }
   package { $other_packages: }
   package { $fonts_packages: }
